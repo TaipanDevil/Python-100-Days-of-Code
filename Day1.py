5b7d3d44e74b8b33c2f7b0119ebb9c331b4a9522
TaipanDevil <167256198+TaipanDevil@users.noreply.github.com>
2025-04-14 16:34:12 -0600

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,6 @@
+# 100 Days of code Project 1: Band Name Generator
+print("Welcome to the band Name Generator.")
+City = input("What is the name of the city you grew up in?\n")
+Pet = input("What is a name of a pet you grew up with?\n")
+
+print("Your band name is: The " + City + " " + Pet)
